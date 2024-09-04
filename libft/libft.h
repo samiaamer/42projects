c21b1a4a30ed04d5da35d2acca9fa2b95867b340
samia abutaleb <abutalebsamia@gmail.com>
2024-09-04 11:52:41 +0300

    .

--- libft/libft.h
+++ libft/libft.h
@@ -28,6 +28,7 @@ int	ft_atoi(const char *nptr);
 char 	*ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
 void    *ft_memset(void *s, int c, size_t n);
+void    ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
