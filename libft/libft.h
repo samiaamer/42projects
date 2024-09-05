4a45dae750ea8ac3e5156666b66173b9b75dfdb5
samia abutaleb <abutalebsamia@gmail.com>
2024-09-05 18:55:17 +0300

    .

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sabutale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 17:31:03 by sabutale          #+#    #+#             */
-/*   Updated: 2024/09/04 15:21:22 by sabutale         ###   ########.fr       */
+/*   Updated: 2024/09/05 16:00:18 by sabutale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ void    *ft_memset(void *s, int c, size_t n);
 void    ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void* dest, const void* src, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);
+void    *ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
