7cbf63ff6b831ea18cee0c71b1b8928a6428f87b
samia <abutalebsamia@gmai.com>
2024-09-06 21:08:19 +0300

    .

--- libft/libft.h
+++ libft/libft.h
@@ -29,6 +29,8 @@ int     ft_memcmp(const void *s1, const void *s2, size_t n);
 char 	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+void    *ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 void    *ft_memset(void *s, int c, size_t n);
 void    ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void* dest, const void* src, size_t n);
