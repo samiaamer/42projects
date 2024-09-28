afee277381f296ddd02b3d0d899204391ac93e56
samia <abutalebsamia@gmai.com>
2024-09-28 21:18:15 +0300

    ..

--- printf/printf.c
+++ printf/printf.c
@@ -20,21 +20,24 @@ int ft_print_char(char c)
   write (1, &c, 1);
   return (1);
 }
-int ft_print_interger(int num)
+int ft_print_integer(int num)
 {
   int count;
 
   count = 0;
-  if (n == -2147483648)
-		return (11);
+  if (num == -2147483648)
+  {
+    write(1, "-2147483648", 11);
+    return (11);
+  }
   if (num < 0)
   {
     write (1, "-", 1);
     num = -num;
-    return (1 + ft_print_interger(num));
+    return (1 + ft_print_integer(num));
   }
   if (num >= 10)
-    count += ft_print_interger(num / 10);
+    count += ft_print_integer(num / 10);
   count += ft_print_char(num % 10 + '0');
   return (count);
 }
@@ -44,15 +47,12 @@ int ft_print_unsigned(unsigned int num)
 
   count = 0;
   if (num >= 10)
-    count += ft_print_interger(num / 10);
+    count += ft_print_unsigned(num / 10);
   count += ft_print_char(num % 10 + '0');
   return (count);
   
 }
-int ft_print_pointer()
-{
-}
-int ft_print_hex(unsigned int num, char specifier)
+int ft_print_hex(unsigned long num, char specifier)
 {
   int count;
   char *hex_dig;
@@ -70,22 +70,35 @@ int ft_print_hex(unsigned int num, char specifier)
   count += ft_print_char(hex_dig[num % 16]);
   return (count);
 }
+int ft_print_pointer(void *ptr)
+{
+  int count;
+
+  count = 0;
+  if(!ptr)
+    return (write(1, "NULL", 4));
+  count += write(1, "0x", 2);
+  count += ft_print_hex((unsigned long)ptr, 'x');
+  return (count);
+}
+
 int handle_format(char format, va_list args)
 {
   if (format == 's')
     return ft_print_string(va_arg(args, char *));
   else if (format == 'c')
     return ft_print_char(va_arg(args, int));
-  else if (format == 'd' || format[i] == 'i')
+  else if (format == 'd' || format == 'i')
     return ft_print_integer(va_arg(args, int));
   else if (format == 'p')
     return ft_print_pointer(va_arg(args, void *));
-  else if (format == 'x' || format[i] == 'X')
-    return ft_print_hex(va_arg(args, unsigned int), format[i]);
+  else if (format == 'x' || format == 'X')
+    return ft_print_hex(va_arg(args, unsigned int), format);
   else if (format == 'u')
     return ft_print_unsigned(va_arg(args, unsigned int));
   else if (format == '%')
-    return write (1, '%', 1);
+    return write (1, "%", 1);
+  return (0);
 }
 
 int	ft_printf(const char *format, ...)
@@ -111,3 +124,18 @@ int	ft_printf(const char *format, ...)
   va_end(args);
   return (count);
 }
+/*
+int main(void)
+{
+    ft_printf("String: %s\n", "Hello, World!");
+    ft_printf("Character: %c\n", 'A');
+    ft_printf("Integer: %d\n", 42);
+    ft_printf("Negative Integer: %d\n", -42);
+    ft_printf("Unsigned Integer: %u\n", 42);
+    ft_printf("Pointer: %p\n", &main);
+    ft_printf("Hexadecimal (lowercase): %x\n", 255);
+    ft_printf("Hexadecimal (uppercase): %X\n", 255);
+    ft_printf("Percent symbol: %%\n");
+
+    return 0;
+}*/
