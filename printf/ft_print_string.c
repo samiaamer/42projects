77a8028c7b463fd313490caf6a57fb73a1ed514d
samia abutaleb <abutalebsamia@gmail.com>
2024-10-02 17:53:43 +0300

    ...

---
+++ printf/ft_print_string.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_string.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabutale <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 16:06:20 by sabutale          #+#    #+#             */
+/*   Updated: 2024/10/01 16:07:24 by sabutale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int     ft_print_string(const char *str)
+{
+        int     count;
+
+        count = 0;
+        if (!str)
+                return (write(1, "(null)", 6));
+        while (str[count])
+        {
+                write (1, &str[count], 1);
+                count++;
+        }
+        return (count);
+}
