8ffe4dde53c0dbf543ef2a22dc74878e48a882d8
samia abutaleb <abutalebsamia@gmail.com>
2025-01-07 19:47:45 +0300

    .

--- core/New folder/so-long.c
+++ core/New folder/so-long.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "minilibx-linux/mlx.h"
+#include <mlx.h>
 
 int main()
 {
@@ -8,8 +8,8 @@ int main()
     mlx_ptr = mlx_init();
     if (mlx_ptr == NULL)
         return (1);
-
-    mlx_destroy_display(mlx_ptr);
+	mlx_new_window(mlx_ptr, 500, 500, "title");
+    mlx_loop(mlx_ptr);
     free(mlx_ptr);
     
 }
---
+++ core/so-long/printf/ft_print_unsigned.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_unsigned.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabutale <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 16:08:57 by sabutale          #+#    #+#             */
+/*   Updated: 2024/10/01 16:09:20 by sabutale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_print_unsigned(unsigned int num)
+{
+	int	count;
+
+	count = 0;
+	if (num >= 10)
+		count += ft_print_unsigned(num / 10);
+	count += ft_print_char(num % 10 + '0');
+	return (count);
+}
