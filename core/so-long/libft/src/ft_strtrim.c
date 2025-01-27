d3df7b8762b6726fa46a1da7434f5bc19db7d596
samia abutaleb <abutalebsamia@gmail.com>
2025-01-27 11:13:21 +0300

    .

---
+++ core/so-long/libft/src/ft_strtrim.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabutale <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 14:03:56 by sabutale          #+#    #+#             */
+/*   Updated: 2024/09/12 14:03:58 by sabutale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		start;
+	int		end;
+	char	*new_str;
+	int		new_size;
+
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	if (!s1 || !set)
+		return (NULL);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end]))
+		end--;
+	new_size = end - start + 1;
+	new_str = malloc(new_size + 1);
+	if (!new_str)
+		return (NULL);
+	ft_memcpy(new_str, &s1[start], new_size);
+	new_str[new_size] = '\0';
+	return (new_str);
+}
