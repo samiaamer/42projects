65d9a935e3dbe9ef3d2f3310ab1652897e01e967
samia <abutalebsamia@gmai.com>
2025-08-11 22:17:17 +0300

    fixing piscine projects

---
+++ piscine/C04/ex02/ft_putnbr.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabutale <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/23 16:08:04 by sabutale          #+#    #+#             */
+/*   Updated: 2024/07/01 11:06:59 by sabutale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char a)
+{
+	write (1, &a, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write (1, "-2147483648", 11);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+/*
+int main(void)
+{
+	ft_putnbr(12345);
+	
+	ft_putnbr(-12345);
+	
+	ft_putnbr(0);
+	
+	ft_putnbr(-2147483648);
+	
+	return (0);
+}*/
---
+++ piscine/C04/ex03/ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabutale <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 14:28:12 by sabutale          #+#    #+#             */
+/*   Updated: 2024/07/01 10:47:00 by sabutale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	res;
+	int	i;
+	int	sign;
+
+	res = 0;
+	i = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * res);
+}
+/*
+int main (void)
+{
+    char str[] = "  ---+--+1234ab567";
+    int val = ft_atoi(str);
+    printf("%d", val);
+    return (0);
+}*/
